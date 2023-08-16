8b46b2bf174870456ca762a456ed8d675832a3fd
albertmariita92 <experthelp92@gmail.com>
2023-08-15 19:12:22 -0700

    2-replace_in_list.py

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-def element_at(my_list, idx): 
-return my_list
-else:
-new_list = my_list[:]
-new_list[idx] = element
-return new_list
+def replace_in_list(my_list, idx, element):
+    if 0 <= idx < len(my_list):
+        my_list[idx] = element
+    return(my_list)
